9491b295937508962000ec2dc900ea97e0366805
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-16 11:25:13 +0100

    Add foreign key

--- database/migrations/9999_02_16_094932_add_foreign_key.php
+++ database/migrations/9999_02_16_094932_add_foreign_key.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,7 +12,18 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('typology_id')
+                ->constrained();
+        });
+
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->foreignId('category_id')
+                ->constrained();
+            $table->foreignId('product_id')
+                ->constrained();
+        });
+
     }
 
     /**
@@ -23,6 +33,13 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_typology_id_foreign');
+        });
+
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->dropForeign('category_product_category_id_foreign');
+            $table->dropForeign('category_product_product_id_foreign');
+        });
     }
 };
